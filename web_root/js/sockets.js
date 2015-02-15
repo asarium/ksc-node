e68f51c390d2322de80bd270d856bfd1fd2eea30
asarium <asarium@gmail.com>
2015-02-15 16:57:49 +0100

    "Improve" test site layout
    I really have no idea how this works...

--- web_root/js/sockets.js
+++ web_root/js/sockets.js
@@ -7,9 +7,21 @@
 {
     $(document).ready(readyFunction);
 
+    function setData(data, contentEl, timestampEl)
+    {
+        timestampEl.text(data.generated);
+
+        contentEl.text(JSON.stringify(data.raw));
+    }
+
     function readyFunction()
     {
-        var container = $("#content");
+        var kscSpan = $("#ksc-content");
+        var kscTimestamp = $("#ksc-timestamp");
+
+        var vafbSpan = $("#vafb-content");
+        var vafbTimestamp = $("#vafb-timestamp");
+
         var domain = window.location.host;
 
         var socket = new WebSocket("ws://" + domain + "/ws");
@@ -26,8 +38,12 @@
 
         socket.onmessage = function (msg)
         {
-            console.log("Received message: " + msg.data);
-            container.text(msg.data);
+            console.log("Received message...");
+
+            var data = JSON.parse(msg.data);
+
+            setData(data.ksc, kscSpan, kscTimestamp);
+            setData(data.vafb, vafbSpan, vafbTimestamp);
         };
 
         socket.onclose = function ()
