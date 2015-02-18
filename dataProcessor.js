0137a02dd8cc8120389545aaa69057c99782ecdb
asarium <asarium@gmail.com>
2015-02-18 19:14:49 +0100

    Reformat file
    Also improve data processing function a bit

--- dataProcessor.js
+++ dataProcessor.js
@@ -8,6 +8,7 @@ var log = require("./logging");
 
 var fs = require("fs");
 var config = require("config");
+var merge = require("merge");
 
 module.exports = function (ksc, vafb, socketServer)
 {
@@ -44,14 +45,16 @@ module.exports = function (ksc, vafb, socketServer)
     var parseDate = function (dateString)
     {
         var parts = dateString.split(':');
-        if (!parts) {
+        if (!parts)
+        {
             return 0;
         }
 
         var partMultipliers = [86400, 3600, 60, 1];
         var ts;
 
-        switch (parts.length) {
+        switch (parts.length)
+        {
             case 3:
                 parts.unshift('0');
                 ts = Math.floor(new Date().setUTCHours(0, 0, 0, 0) / 1000);
@@ -64,110 +67,141 @@ module.exports = function (ksc, vafb, socketServer)
                 return 0;
         }
 
-        for (var i = 0; i < parts.length; i++) {
+        for (var i = 0; i < parts.length; i++)
+        {
             ts += (parseInt(parts[i], 10) * partMultipliers[i]);
         }
 
         return ts;
     };
-    var setKeys = function (pad)
+
+    var processRawData = function (rawData)
     {
-        var i, j, events = [], rawData = outputObject[pad].raw;
-        outputObject[pad].times = {};
+        var events = [];
+        var out = {};
+        out.times = {};
 
-        if (rawData['GMTTSTRG01']) {
-            outputObject[pad].times.gmt = parseDate(rawData['GMTTSTRG01']);
+        if (rawData['GMTTSTRG01'])
+        {
+            out.times.gmt = parseDate(rawData['GMTTSTRG01']);
         }
-        if (rawData['LOCLSTRG02']) {
-            outputObject[pad].tz = rawData['LOCLSTRG02'];
+
+        if (rawData['LOCLSTRG02'])
+        {
+            out.tz = rawData['LOCLSTRG02'];
         }
-        if (rawData['LOCTSTRG03']) {
-            outputObject[pad].times.local = parseDate(rawData['LOCTSTRG03']);
+
+        if (rawData['LOCTSTRG03'])
+        {
+            out.times.local = parseDate(rawData['LOCTSTRG03']);
         }
-        if (rawData['BIHTSTRG04']) {
-            outputObject[pad].hold = rawData['BIHTSTRG04'];
+
+        if (rawData['BIHTSTRG04'])
+        {
+            out.hold = rawData['BIHTSTRG04'];
         }
-        if (rawData['WOTTSTRG06']) {
-            outputObject[pad].times.windowOpens = parseDate(rawData['WOTTSTRG06']);
+
+        if (rawData['WOTTSTRG06'])
+        {
+            out.times.windowOpens = parseDate(rawData['WOTTSTRG06']);
         }
-        if (rawData['ELOTSTRG07']) {
-            outputObject[pad].times.expected = parseDate(rawData['ELOTSTRG07']);
+
+        if (rawData['ELOTSTRG07'])
+        {
+            out.times.expected = parseDate(rawData['ELOTSTRG07']);
         }
-        if (rawData['ALOLSTRG08'] && rawData['ALOTSTRG09']) {
-            if (!outputObject[pad].times.custom) {
-                outputObject[pad].times.custom = [];
+
+        if (rawData['ALOLSTRG08'] && rawData['ALOTSTRG09'])
+        {
+            if (!out.times.custom)
+            {
+                out.times.custom = [];
             }
-            outputObject[pad].times.custom.push({
-                label: rawData['ALOLSTRG08'],
-                time: parseDate(rawData['ALOTSTRG09'])
-            });
+            out.times.custom.push({
+                                      label: rawData['ALOLSTRG08'],
+                                      time : parseDate(rawData['ALOTSTRG09'])
+                                  });
         }
-        if (rawData['LTMLSTRG10'] && rawData['LMTTSTRG11']) {
-            if (!outputObject[pad].times.custom) {
-                outputObject[pad].times.custom = [];
+
+        if (rawData['LTMLSTRG10'] && rawData['LMTTSTRG11'])
+        {
+            if (!out.times.custom)
+            {
+                out.times.custom = [];
             }
-            outputObject[pad].times.custom.push({
-                label: rawData['LTMLSTRG10'],
-                time: parseDate(rawData['LTMTSTRG11'])
-            });
+            out.times.custom.push({
+                                      label: rawData['LTMLSTRG10'],
+                                      time : parseDate(rawData['LTMTSTRG11'])
+                                  });
         }
-        if (rawData['TTMLSTRG12'] && rawData['TTMTSTRG13']) {
-            if (!outputObject[pad].times.custom) {
-                outputObject[pad].times.custom = [];
+
+        if (rawData['TTMLSTRG12'] && rawData['TTMTSTRG13'])
+        {
+            if (!out.times.custom)
+            {
+                out.times.custom = [];
             }
-            outputObject[pad].times.custom.push({
-                label: rawData['TTMLSTRG12'],
-                time: parseDate(rawData['TTMTSTRG13'])
-            });
+            out.times.custom.push({
+                                      label: rawData['TTMLSTRG12'],
+                                      time : parseDate(rawData['TTMTSTRG13'])
+                                  });
         }
-        if (rawData['RTMLSTRG14'] && rawData['RTMTSTRG15']) {
-            if (!outputObject[pad].times.custom) {
-                outputObject[pad].times.custom = [];
+
+        if (rawData['RTMLSTRG14'] && rawData['RTMTSTRG15'])
+        {
+            if (!out.times.custom)
+            {
+                out.times.custom = [];
             }
-            outputObject[pad].times.custom.push({
-                label: rawData['RTMLSTRG14'],
-                time: parseDate(rawData['RTMTSTRG15'])
-            });
+            out.times.custom.push({
+                                      label: rawData['RTMLSTRG14'],
+                                      time : parseDate(rawData['RTMTSTRG15'])
+                                  });
         }
-        if (rawData['VHCLSTRG16']) {
-            outputObject[pad].vehicle = rawData['VHCLSTRG16'];
+
+        if (rawData['VHCLSTRG16'])
+        {
+            out.vehicle = rawData['VHCLSTRG16'];
         }
-        if (rawData['SPCFSTRG17']) {
-            outputObject[pad].spacecraft = rawData['SPCFSTRG17'];
+
+        if (rawData['SPCFSTRG17'])
+        {
+            out.spacecraft = rawData['SPCFSTRG17'];
         }
-        for (var i = 1; i < 11; i++) {
-            j = ("0" + i).slice(-2);
-            if (rawData['EVENTLBL' + j] && rawData['EVENTLBL' + j].length) {
+
+        for (var i = 1; i < 11; i++)
+        {
+            var j = ("0" + i).slice(-2);
+            if (rawData['EVENTLBL' + j] && rawData['EVENTLBL' + j].length)
+            {
                 events.push({
-                    label: rawData['EVENTLBL' + j],
-                    time: rawData['EVENTTIM' + j]
-                });
+                                label: rawData['EVENTLBL' + j],
+                                time : rawData['EVENTTIM' + j]
+                            });
             }
         }
-        if (events.length) {
-            outputObject[pad].events = events;
+        if (events.length)
+        {
+            out.events = events;
         }
 
-        outputObject[pad].generated = Math.floor(Date.now() / 1000);
+        out.generated = Math.floor(Date.now() / 1000);
+
+        return out;
     };
 
-    ksc.onData(function (data)
-               {
-                   for (var i in data.raw) {
-                       outputObject.ksc.raw[i] = data.raw[i];
-                   }
-                   setKeys('ksc');
-                   socketServer.sendToAll(outputObject);
-                   writeToLegacy();
-               });
+    var getListener = function (key)
+    {
+        return function (data)
+        {
+            var processed = processRawData(data.raw);
+            outputObject[key] = merge(processed, data); // data already contains the generated field
+            socketServer.sendToAll(outputObject);
+            writeToLegacy();
+        };
+    };
 
-    vafb.onData(function (data)
-                {
-                    for (var i in data.raw) {
-                        outputObject.vafb.raw[i] = data.raw[i];
-                    }
-                    setKeys('vafb');
-                    socketServer.sendToAll(outputObject);
-                    writeToLegacy();
-                })
+    ksc.onData(getListener("ksc"));
+
+    vafb.onData(getListener("vafb"))
 };
